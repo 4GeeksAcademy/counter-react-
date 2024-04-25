3e51bd41165b4f1c07fa92d97f79da596ea3d878
valeriaasalcedoo <153454325+valeriaasalcedoo@users.noreply.github.com>
2024-04-25 12:15:58 +0000

    listo con los botones

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import '../../styles/index.css';
+
+const SecondsCounter = ({ seconds }) => {
+  const [counter, setCounter] = useState(seconds);
+  const [digits, setDigits] = useState(["0", "0", "0", "0", "0", "0"]);
+  const [running, setRunning] = useState(true);
+
+  useEffect(() => {
+    let interval;
+    if (running) {
+      interval = setInterval(() => {
+        setCounter((prevSecond) => (prevSecond === 999999 ? 0 : prevSecond + 1));
+      }, 1000);
+    }
+
+    return () => clearInterval(interval);
+  }, [running]);
+
+  useEffect(() => {// 1  >> "1".padStart(6,"0")  >>>   "000001"   >>  523 >>>  "000523"  75623    "075623"
+    let newDigit = counter.toString().padStart(6, "0").split("");
+    setDigits(newDigit);
+  }, [counter]);
+
+  const handleReset = () => {
+    setCounter(seconds);
+  };
+
+  const handleStop = () => {
+    setRunning(false);
+  };
+
+  const handleStart = () => {
+    setRunning(true);
+  };
+
+  return (
+    <div className="counter-container">
+      <div className="counter">
+        <div className="digit">{digits[0]}</div>
+        <div className="digit">{digits[1]}</div>
+        <div className="digit">{digits[2]}</div>
+        <div className="digit">{digits[3]}</div>
+        <div className="digit">{digits[4]}</div>
+        <div className="digit">{digits[5]}</div>
+      </div>
+      <div className="buttons">
+        <button onClick={handleReset} className="button-reset">Reset</button>
+        <button onClick={handleStop} className="button-stop">Stop</button>
+        <button onClick={handleStart} className="button-start">Start</button>
+      </div>
+    </div>
+
+  );
+};
+
+export default SecondsCounter;
